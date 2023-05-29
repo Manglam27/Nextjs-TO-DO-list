c90845dd9796e1adff3eb5ff649645b4772eaaaa
Manglam Patel <patelmanglam27@gmail.com>
2023-05-29 21:00:37 +0530

    It Works!

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,19 +16,26 @@ async function toggleTodo (id, completed) {
         data: { completed: completed },
     })
 }
+
+async function deleteTodo (id) {   
+    "use server"
+
+    await prisma.Todo.delete({
+        where: { id },
+    })
+}
+
+
 const Todo = async() => {
     const Todo_data = await getTodos()
     return (
         <div>
-            <CustomMenu/>
+            <CustomMenu deleteTodo={deleteTodo}/>
             <div className="flex justify-between items-center text-4xl md:text-5xl ">
                 <h1>TO-DO</h1>
                 <button><Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">Create New</Link></button>
             </div>
-            <div className="flex justify-end items-center text-4xl md:text-5xl mt-5 md:mt-8">
-            <button className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" >Remove Old</button>
-            </div>
-            <div className=" mt-[0px] pl-5" >
+            <div className=" mt-[110px] pl-5" >
                 <ul>
                     {Todo_data.map((Todo) => (
                         <Todoitem key={Todo.id} {...Todo} toggleTodo={toggleTodo}/>
---
+++ src/components/CustomMenu.jsx
@@ -0,0 +1,82 @@
+"use client";
+import React, { useEffect } from 'react';
+import '@/app/globals.css'
+
+
+const CustomMenu = ({deleteTodo}) => {
+    const menuRef = React.useRef(null);
+    const menuState = {
+        visible: false,
+        x: 0,
+        y: 0,
+    };
+
+    const showMenu = (x, y) => {
+        menuState.visible = true;
+        menuState.x = x;
+        menuState.y = y;
+        menuRef.current.style.display = 'block';
+        menuRef.current.style.left = `${x}px`;
+        menuRef.current.style.top = `${y}px`;
+    };
+
+    const hideMenu = () => {
+        menuState.visible = false;
+        menuRef.current.style.display = 'none';
+    };
+
+    let forAttributeValue  = '';
+
+    const handleContextMenu = (event) => {
+        event.preventDefault();
+        showMenu(event.clientX, event.clientY);
+        window.addEventListener('click', handleWindowClick);
+
+
+        const targetComponent = event.target;
+        forAttributeValue = targetComponent.getAttribute('for');
+    };
+
+    const handleWindowClick = (event) => {
+        if (!event.target.classList.contains('menu-item')) {
+            hideMenu();
+        }
+    };
+
+    const handleMenuItemClick = (event) => {
+        console.log('clicked', event.target);
+    
+        if (event.target.innerHTML === 'Delete') {
+            console.log('Component ID:', forAttributeValue);
+            if(forAttributeValue === null){ return; }
+            deleteTodo(forAttributeValue);
+            
+        }
+        hideMenu();
+    };
+
+    useEffect(() => {
+        window.addEventListener('contextmenu', handleContextMenu);
+        return () => {
+            window.removeEventListener('contextmenu', handleContextMenu);
+            window.removeEventListener('click', handleWindowClick);
+        };
+    }, []);
+
+    return (
+        <div
+            id="menu"
+            ref={menuRef}
+            className="menu"
+            style={{
+                display: 'none',
+                position: 'absolute',
+            }}
+        >
+            <div className="menu-item" onClick={handleMenuItemClick}>Copy</div>
+            <div className="menu-item" onClick={handleMenuItemClick}>Delete</div>
+        </div>
+    );
+};
+
+export default CustomMenu;
